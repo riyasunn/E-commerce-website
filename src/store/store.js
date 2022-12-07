7e7592c5de2e1b5e25f5de238022d33436d95ff5
Riya Sun <riya.sunn@gmail.com>
2022-12-07 14:24:06 +1300

    delete user.saga.js & root-saga

--- src/store/store.js
+++ src/store/store.js
@@ -4,8 +4,6 @@ import { rootReducer } from './root-reducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
-import createSagaMiddleware from 'redux-saga';
-import { rootSaga } from './root-saga';
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +11,6 @@ const persistConfig = {
     whitelist: ['cart'],
 }
 
-// const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
@@ -25,6 +22,5 @@ const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
-// sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
