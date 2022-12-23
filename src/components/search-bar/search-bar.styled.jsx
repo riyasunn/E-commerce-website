e8f699fea815924a2a26141660846485f1687db9
Riya Sun <riya.sunn@gmail.com>
2022-12-23 16:33:49 +1300

    clear default css properties of the browser for the search bar

--- src/components/search-bar/search-bar.styled.jsx
+++ src/components/search-bar/search-bar.styled.jsx
@@ -9,12 +9,20 @@ export const SearchContainer = styled.div`
    input{
     width:250px;
     height:2rem;
+    border-radius: 5px;
+    border: 1px solid grey;
    }
    input[type="search"] {
     font-size: 18px;
     padding-left: 1rem;
    }
    input::-webkit-search-cancel-button {
+      display: none;
+   }
+   input[type="search"] {
+      -webkit-appearance: none;
+   }
+   input[type="search"] ::-webkit-search-decoration {
       display: none;
    }
    .clear-button {
@@ -29,14 +37,20 @@ export const SearchContainer = styled.div`
       input{
          width: 150px;
          height: 40px;
+         border-radius: 5px;
+         border: 1px solid grey;
+         background-color: #f5f5f5;
       }
       input[type="search"] {
          font-size: 15px;
          padding-left: .5rem;
       }
+      
+
       .clear-button {
          position: absolute;
          margin-left:120px;
+         /* margin-right: 4.5rem; */
          cursor: pointer;
       }
    }
